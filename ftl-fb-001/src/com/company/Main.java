ded39cd5c830db8148295db4e4234cf1299d5efd
Никита Тарасов` <nikita.taras0ff@yandex.ru>
2022-06-18 12:56:41 +0300

    L006 - static/instance methods example

--- ftl-fb-001/src/com/company/Main.java
+++ ftl-fb-001/src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 import com.company.L001.HelloWorld;
 import com.company.L002.POJO;
 import com.company.animals.Cat;
+import com.company.animals.Dog;
 
 public class Main {
     public static void main(String[] args) {
@@ -16,5 +17,11 @@ public class Main {
 //        catL004.voice();
         Cat cat = new Cat("Murzik", 20);
         System.out.println(cat.jumpHeight());
+
+        Dog dog = new Dog();
+        dog.goToStick(14);
+        dog.whereDog();
+        Dog homelessdog = Dog.ofHomeless(100);
+
     }
 }
