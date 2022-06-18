3a424d94f6c69be66e86695f5fa20e27a471a603
Никита Тарасов` <nikita.taras0ff@yandex.ru>
2022-06-18 19:56:58 +0300

    L009 - if/else example

--- ftl-fb-001/src/com/company/animals/Duck.java
+++ ftl-fb-001/src/com/company/animals/Duck.java
@@ -7,4 +7,11 @@ public class Duck extends Bird{
     public void voice() {
         System.out.println("Quack!");
     }
+
+    public void showSpeed(){
+        if(this.flyModActive == true)
+            System.out.println("40");
+        else
+            System.out.println("4");
+    }
 }
