e0eb37864a9eb8e7ab81e0d0f9f32292ea3cd78d
Никита Тарасов` <nikita.taras0ff@yandex.ru>
2022-06-28 13:43:31 +0300

    T001 - example

---
+++ ftl-fb-001/src/com/company/T001/PrintNumbers.java
@@ -0,0 +1,13 @@
+package com.company.T001;
+
+public class PrintNumbers {
+
+    private static void runPrintNumbers(){
+        for(int i = 1; i <= 5; i++)
+            System.out.println(i);
+    }
+
+    public static void run(){
+        runPrintNumbers();
+    }
+}
