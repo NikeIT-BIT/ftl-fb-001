c04899c02312ae1758e427ad6cd93d3636eaa2ee
Никита Тарасов` <nikita.taras0ff@yandex.ru>
2022-06-28 14:55:21 +0300

    T005 - example

--- ftl-fb-001/src/com/company/T005/PrintNumbersMultiplierSeven.java
+++ ftl-fb-001/src/com/company/T005/PrintNumbersMultiplierSeven.java
@@ -1,2 +1,19 @@
-package com.company.T005;public class PrintNumbersMultiplierSeven {
+package com.company.T005;
+
+public class PrintNumbersMultiplierSeven {
+
+    private static void runPrintNumbers(){
+
+        Integer number = 0;
+
+        for(int i = 1; number < 98; i++) {
+            number = 7 * i;
+            System.out.printf("%d ", number);
+        }
+    }
+
+    public static void run(){
+        runPrintNumbers();
+    }
+
 }
