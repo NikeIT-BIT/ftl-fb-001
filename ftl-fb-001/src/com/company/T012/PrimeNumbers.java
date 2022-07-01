80f6c0d0c160d75741b26b96958c96a50989a335
Никита Тарасов` <nikita.taras0ff@yandex.ru>
2022-07-01 11:53:37 +0300

    T013 - example + T012 - edit

--- ftl-fb-001/src/com/company/T012/PrimeNumbers.java
+++ ftl-fb-001/src/com/company/T012/PrimeNumbers.java
@@ -3,11 +3,15 @@ package com.company.T012;
 public class PrimeNumbers {
 
     private static void runPrimeNumbers(){
-        for(int i = 2; i < 100; i++){
-            if(i % 2 != 0 || i == 2)
-                if(i % 3 != 0 || i == 3) System.out.printf("%d ", i);
+        for(int i = 2; i <= 100; i++){
+            Boolean primeNumber = false;
+            Integer divider = 1;
+            for(int j = 2; j <= i; j++){
+                if(i % j == 0) divider++;
+            }
+            if (divider == 2) primeNumber = true;
+            if (primeNumber) System.out.printf("%d ", i);
         }
-        System.out.println();
     }
 
     public static void run(){
